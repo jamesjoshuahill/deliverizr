bae1b22269dbb09c860758e60c6a5f3183d15a65
Josh Hill <jhill@pivotal.io>
2016-05-06 10:14:22 +0100

    Start Spring Boot web project
    Signed-off-by: David Evans <devans@pivotal.io>

---
+++ src/main/java/io/pivotal/DeliverizrApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverizrApplication.class, args);
+	}
+}
